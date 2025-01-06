62ea1b0d87874dd5a848e0526c8d6e3c58e44548
simonpcouch <simonpatrickcouch@gmail.com>
2025-01-06 16:49:55 -0600

    move focus back to editor after streaming in Positron (closes #28)

--- R/addin.R
+++ R/addin.R
@@ -45,6 +45,10 @@ gander_addin <- function() {
     input = input, assistant = assistant, contents = contents
   )
 
+  if (is_positron()) {
+    .ps.ui.executeCommand("workbench.action.focusActiveEditorGroup")
+  }
+  
   cli::cli_inform(
     "Use {.run gander::gander_undo()} or {.run gander::gander_retry()} if needed."
   )

fd04d9aaff510a6a5b6755bd2813669324a26d8f
simonpcouch <simonpatrickcouch@gmail.com>
2025-05-21 16:14:43 -0500

    scrub provider

--- inst/chat/chat.R
+++ inst/chat/chat.R
@@ -3,4 +3,10 @@ ch <- ellmer::chat_anthropic()
 
 ch$chat("What's 2+2?")
 
+ch$.__enclos_env__$private$provider <- ellmer::Provider(
+  "Anthropic",
+  "claude-3-7-sonnet-latest",
+  "example.org"
+)
+
 saveRDS(ch, file = "inst/chat/chat.rds")
